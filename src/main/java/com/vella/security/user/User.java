c3aca33af3903a0c13c63eecd79b5a88a5815fea
vella <vadmir04@gmail.com>
2023-07-01 15:39:56 +0200

    Typo fixed

--- src/main/java/com/vella/security/user/User.java
+++ src/main/java/com/vella/security/user/User.java
@@ -22,7 +22,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "_user")
+@Table(name = "user")
 public class User implements UserDetails {
 
   @Id
