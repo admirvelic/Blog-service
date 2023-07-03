ba87adee92eb2239d968730bcf84ce244238109e
vella <vadmir04@gmail.com>
2023-07-03 23:19:22 +0200

    @NonNullApi annotation remove

--- src/main/java/com/vella/blog/repository/BlogRepo.java
+++ src/main/java/com/vella/blog/repository/BlogRepo.java
@@ -2,10 +2,9 @@ package com.vella.blog.repository;
 
 import com.vella.blog.model.Blog;
 import java.util.Optional;
-import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@NonNullApi
+
 public interface BlogRepo extends JpaRepository<Blog, Long> {
 
   @Override
